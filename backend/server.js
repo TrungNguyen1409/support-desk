c1d1f26c2ff9da01fbce070c9e48f0e89ebcebad
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-12-20 01:35:25 +0100

    add simple server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server is running...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000;
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.status(200).json({message: "Welcome to the support desk API"})
+})
+
+app.get('/users', (req,res) => {
+    res.status(200).json({user: "User1"})
+})
+
+app.listen(PORT, () => console.log(`Server starts on port ${PORT}`))
