0a5c6a7a361a09fe516b58a921dd8655ed3853d4
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-12-22 12:29:13 +0100

    starting with register page, input fiel for name

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,48 @@
+import {useState} from 'react'
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {name, email, password, password2} = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) =>({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser/> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id='name' 
+              name = 'name'
+              value={name} 
+              onChange={onChange} 
+              placeholder='Enter you name' 
+            />
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
