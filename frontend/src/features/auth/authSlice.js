fa9172ad7766eb44118697c6bedb8b05542ef352
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2023-01-02 22:04:29 +0100

    save progress: unresolve problem. Webpack5

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-
+import authService from './authService'
 
 const initialState= {
     user: null,
@@ -9,8 +9,16 @@ const initialState= {
     message: '',
 }
 
-export const register = createAsyncThunk('auth/register', async (user,thunkAPI) => {
-    console.log(user)
+export const register = createAsyncThunk(
+    'auth/register', 
+    async (user,thunkAPI) => {
+    try{
+        return await authService.register(user)
+    }catch(error){
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        
+        return thunkAPI.rejectWithValue(message)
+    }
 })
 
 export const login = createAsyncThunk('auth/login', async (user,thunkAPI) => {
